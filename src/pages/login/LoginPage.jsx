4e9babb11acef2d8ff7f67c69ff61ddbbe512885
domvivas123 <nobelavivaldi@gmail.com>
2025-02-12 19:53:43 +0200

    fix login page

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLogin } from '../../hooks/useLogin';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,14 +8,15 @@ function LoginPage() {
     const [password, setPassword] = useState('');
     const { loading, error, data, handleLogin } = useLogin();
     
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        await handleLogin(email, password);
-
-
+    useEffect(() => {
         if (data) {
             navigate('/dashboard');
         }
+    }, [data, navigate]); // Navegar assim que 'data' for atualizado
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await handleLogin(email, password);
     };
 
     return (
@@ -65,7 +66,22 @@ function LoginPage() {
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             disabled={loading}
                         >
-                            {loading ? 'Signing in...' : 'Sign in'}
+                            {loading ? (
+                                <svg
+                                    role="status"
+                                    className="w-5 h-5 text-blue-800 animate-spin"
+                                    viewBox="0 0 100 101"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M100 50.5C100 78.4 78.4 100 50 100C21.6 100 0 78.4 0 50.5C0 22.6 21.6 1 50 1C78.4 1 100 22.6 100 50.5ZM91 50.5C91 72.6 72.6 91 50 91C27.4 91 9 72.6 9 50.5C9 28.4 27.4 9 50 9C72.6 9 91 28.4 91 50.5Z"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                            ) : (
+                                'Sign in'
+                            )}
                         </button>
                     </div>
 
